d1d34fdfc948dac38f054599f9701589d2908987
HoangAnh220302 <127639155+HoangAnh220302@users.noreply.github.com>
2023-04-01 23:08:07 +0700

    Add files via upload

---
+++ Bai 1_Tuan 3/Bai 4/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bai_4
+{
+    class Person
+    {
+        public string ID { get; set;}
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+
+        public void CheckAge()
+        {
+            if (this.Age >= 18)
+            {
+                Console.WriteLine("Ban da du tuoi bau cu!");
+            }
+            else
+            {
+                Console.WriteLine("Ban chua du tuoi bau cu!");
+            }
+        }
+    }
+}
---
+++ Bai 1_Tuan 3/Bai 4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Bai_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person A = new Person();
+            A.ID = "ABC";
+            A.Name = "Hahaha";
+            A.Age = 19;
+            A.Email = "pvzbigfans@gmail.com";
+            A.Address = "Homeless";
+
+            Console.WriteLine("Person A Info");
+            Console.WriteLine($"ID = {A.ID}");
+            Console.WriteLine($"Name = {A.ID}");
+            Console.WriteLine($"Age = {A.ID}");
+            Console.WriteLine($"Email = {A.ID}");
+            Console.WriteLine($"Address = {A.Address}");
+
+            A.CheckAge();
+
+
+        }
+    }
+}
