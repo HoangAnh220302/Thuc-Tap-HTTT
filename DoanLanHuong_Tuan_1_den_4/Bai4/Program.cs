b4e768c6848f1c4c07d200939c53c71aefe3150a
HoangAnh220302 <127639155+HoangAnh220302@users.noreply.github.com>
2023-04-16 02:45:13 +0700

    Add files via upload

---
+++ DoanLanHuong_Tuan_1_den_4/Bai4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Bai4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Nhap so nguyen duong n: ");
+            n = int.Parse(Console.ReadLine());
+            long tong = 0;
+            /*
+            for(int i = 0; i <= n; i++)
+            {
+                tong += i;
+            }
+            Console.WriteLine("Tong la:{0} ", tong);*/
+
+            Console.ReadLine();
+        }
+    }
+}
